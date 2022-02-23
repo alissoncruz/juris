d21f953a1961e68e644497c1281440ae0b37be7e
Jeremias-2000 <jdossantospinheiro20@gmail.com>
2022-02-23 16:14:23 -0300

    adicionando regras de acesso

--- src/main/java/com/juris/config/securiry/WebSecurityConfig.java
+++ src/main/java/com/juris/config/securiry/WebSecurityConfig.java
@@ -1,14 +1,23 @@
 package com.juris.config.securiry;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static com.juris.config.securiry.WebSecurityConfig.ApplicationUserRole.*;
 
 @Configuration
 @EnableWebSecurity
@@ -40,12 +49,35 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        // cria uma conta default
-        auth.inMemoryAuthentication()
-                .withUser("admin")
-                .password(passwordEncoder.encode("password"))
-                .roles("ADMIN");
-    }
+   @Override
+    @Bean
+    protected UserDetailsService userDetailsService(){
+      UserDetails administrator =  User.builder()
+               .username("alisson")
+               .password(passwordEncoder.encode("juris@xp2022"))
+               .roles(ADMIN.name())
+               .build();
+      UserDetails lawyer = User.builder()
+              .username("advogado")
+              .password(passwordEncoder.encode("adv@12345"))
+              .roles(LAWYER.name())
+              .build();
+      UserDetails attendant = User.builder()
+              .username("atendente")
+              .password(passwordEncoder.encode("atendente@123"))
+              .roles(ATTENDANT.name())
+              .build();
+      return new InMemoryUserDetailsManager(
+              administrator,
+              lawyer,
+              attendant
+      );
+   }
+   @Getter
+   @AllArgsConstructor
+   public enum ApplicationUserRole{
+        ADMIN,
+       LAWYER,
+       ATTENDANT
+   }
 }
